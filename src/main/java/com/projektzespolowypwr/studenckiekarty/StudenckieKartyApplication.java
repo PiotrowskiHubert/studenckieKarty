74c4b09a259a781de1f01a7301aa0006498cc0ec
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-03-19 14:25:50 +0100

    initial commit

---
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
@@ -0,0 +1,13 @@
+package com.projektzespolowypwr.studenckiekarty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenckieKartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudenckieKartyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projektzespolowypwr.studenckiekarty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenckieKartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
