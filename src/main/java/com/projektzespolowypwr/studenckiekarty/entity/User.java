7a78946ca9fd7c2d2d895b7a4d3d501f1a86480b
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-17 20:24:44 +0200

    add entity, data, builder, constructor annotations

--- src/main/java/com/projektzespolowypwr/studenckiekarty/entity/User.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/entity/User.java
@@ -1,10 +1,20 @@
 package com.projektzespolowypwr.studenckiekarty.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
