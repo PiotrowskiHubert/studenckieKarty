00d67c22f91b2eef353297972ed26c15764518df
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-17 20:41:11 +0200

    add JpaRepository extension

--- src/main/java/com/projektzespolowypwr/studenckiekarty/repository/IUserRepository.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/repository/IUserRepository.java
@@ -1,7 +1,9 @@
 package com.projektzespolowypwr.studenckiekarty.repository;
 
+import com.projektzespolowypwr.studenckiekarty.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserRepository {
+public interface IUserRepository extends JpaRepository<User, Long> {
 }
