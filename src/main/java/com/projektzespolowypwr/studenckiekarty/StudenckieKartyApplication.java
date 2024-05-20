492ed842931f75e2534b5a1b27ef98c3fab12dac
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 21:01:08 +0200

    clean up

--- src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
@@ -1,23 +1,14 @@
 package com.projektzespolowypwr.studenckiekarty;
 
-import com.projektzespolowypwr.studenckiekarty.repository.IUserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudenckieKartyApplication implements CommandLineRunner {
-
-    private final IUserRepository userRepository;
-
-    public StudenckieKartyApplication(IUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(StudenckieKartyApplication.class, args);
     }
-
 
     @Override
     public void run(String... args) throws Exception {
