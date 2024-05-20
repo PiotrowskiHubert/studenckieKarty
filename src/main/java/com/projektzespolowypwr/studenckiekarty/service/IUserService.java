12503741881d48cb96dad6c70fd8a641402f2507
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 21:02:19 +0200

    add addUser and add findUserByNickname

--- src/main/java/com/projektzespolowypwr/studenckiekarty/service/IUserService.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/service/IUserService.java
@@ -1,4 +1,15 @@
 package com.projektzespolowypwr.studenckiekarty.service;
 
+import com.projektzespolowypwr.studenckiekarty.dto.UserDto;
+import com.projektzespolowypwr.studenckiekarty.entity.MyUser;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface IUserService {
+
+    @Transactional
+    MyUser add(UserDto newUser);
+
+    MyUser findUserByNickname(String nickname);
 }
