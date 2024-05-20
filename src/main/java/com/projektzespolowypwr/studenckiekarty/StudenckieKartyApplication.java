8758e2d107d7fa0f124bb14f5d8f57684cd7b8aa
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 17:47:21 +0200

    add imports

--- src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
@@ -1,7 +1,11 @@
 package com.projektzespolowypwr.studenckiekarty;
 
+import com.projektzespolowypwr.studenckiekarty.entity.User;
+import com.projektzespolowypwr.studenckiekarty.repository.IUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class StudenckieKartyApplication implements CommandLineRunner {
