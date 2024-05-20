5f3603b51281eac3634f4c8ce8c713b620868a77
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 17:24:32 +0200

    add CommandLineRunner

--- src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/StudenckieKartyApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudenckieKartyApplication {
+public class StudenckieKartyApplication implements CommandLineRunner {
+
+    private final IUserRepository userRepository;
+
+    public StudenckieKartyApplication(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudenckieKartyApplication.class, args);
