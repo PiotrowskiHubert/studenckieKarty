0796fd90960d5dc1829de37920f9e58f8532d185
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 17:26:06 +0200

    add userRepository

--- src/main/java/com/projektzespolowypwr/studenckiekarty/service/UserDetailService.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/service/UserDetailService.java
@@ -1,4 +1,13 @@
 package com.projektzespolowypwr.studenckiekarty.service;
 
+import com.projektzespolowypwr.studenckiekarty.repository.IUserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserDetailService {
+    private final IUserRepository userRepository;
+
+    public UserDetailService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
