2ebf997274393fac9a5302d3c6fa37dd3d00f631
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-20 17:29:12 +0200

    add UserDetailService

--- src/main/java/com/projektzespolowypwr/studenckiekarty/service/UserDetailService.java
+++ src/main/java/com/projektzespolowypwr/studenckiekarty/service/UserDetailService.java
@@ -1,13 +1,21 @@
 package com.projektzespolowypwr.studenckiekarty.service;
 
 import com.projektzespolowypwr.studenckiekarty.repository.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailService {
+public class UserDetailService implements UserDetailsService {
     private final IUserRepository userRepository;
 
     public UserDetailService(IUserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findBuUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
